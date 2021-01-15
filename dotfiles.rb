04b152df668f94909d1118f09684e89ebc2cfb04
chenjian <com.me@foxmail.com>
2021-01-15 09:55:49 +0800

    add dotfiles

---
+++ dotfiles.rb
@@ -0,0 +1,17 @@
+class Dotfiles < Formula
+  desc "Symbolic links management CLI"
+  homepage "https://github.com/rhysd/dotfiles"
+  url "https://github.com/rhysd/dotfiles/archive/v0.2.2.tar.gz"
+  sha256 "090b918636b405be8dab6d19e541afda2bf0e252409bff284679b297d3e563e4"
+  license "MIT"
+
+  depends_on "go" => :build
+
+  def install
+    system "go", "build", *std_go_args, "."
+  end
+
+  test do
+    system "dotfiles", "version"
+  end
+end
