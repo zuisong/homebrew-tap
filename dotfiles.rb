653439c3c95f13eeabe7274012ed9550fdc2b210
chenjian <com.me@foxmail.com>
2021-08-02 10:39:43 +0800

    dotfiles live check

--- dotfiles.rb
+++ dotfiles.rb
@@ -11,6 +11,11 @@ class Dotfiles < Formula
     system "go", "build", *std_go_args, "."
   end
 
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
+
   test do
     system "dotfiles", "version"
   end
