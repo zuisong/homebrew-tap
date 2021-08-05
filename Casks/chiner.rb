dbf54cc8d1e5a8f4deb5612d4b890d424518365d
chenjian <com.me@foxmail.com>
2021-08-05 09:43:01 +0800

    add chiner livecheck

--- Casks/chiner.rb
+++ Casks/chiner.rb
@@ -1,9 +1,18 @@
 cask "chiner" do
   version "3.0.2"
   sha256 "b24d21d96380a221a2abec0453a0db64ad4c4389efd451c88f792b35233dcbb7"
-  url "http://chiner-release.httpchk.com/CHINER-mac_v#{version}.dmg"
+
+  url "http://chiner-release.httpchk.com/CHINER-mac_v#{version}.dmg",
+      verified: "chiner-release.httpchk.com/"
   name "Chiner"
+  desc "Database model tools"
   homepage "https://gitee.com/robergroup/chiner"
+
+  livecheck do
+    url "https://gitee.com/robergroup/chiner"
+    strategy :page_match
+    regex(%r{.*/robergroup/chiner/releases/v([.\d]+)"}i)
+  end
 
   app "CHINER元数建模.app", target: "Chiner.app"
 
@@ -12,5 +21,4 @@ cask "chiner" do
     "~/Library/Saved Application State/chiner.savedState",
     "~/Library/Preferences/chiner.plist",
   ]
-
 end
