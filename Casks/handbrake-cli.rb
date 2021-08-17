4f72009047271e524fd5071bb80a563b59ed998d
chenjian <com.me@foxmail.com>
2021-08-17 10:14:35 +0800

    Update handbrake-cli from 1.4.0 to 1.4.1

--- Casks/handbrake-cli.rb
+++ Casks/handbrake-cli.rb
@@ -1,6 +1,6 @@
 cask "handbrake-cli" do
-  version "1.4.0"
-  sha256 "17e51c52f29142b94c49c744afe2b4b80c20176309cd04a2234b79f10f35f1f5"
+  version "1.4.1"
+  sha256 "4af30f966856469a360b9b3ae413ce01927cc57687f9892dbbc173ca13ca3b72"
 
   url "https://github.com/HandBrake/HandBrake/releases/download/#{version}/HandBrakeCLI-#{version}.dmg",
       verified: "github.com/HandBrake/HandBrake/"
