4da0e9e2439c0d7c821cbc2278ba4c314a6cdc77
chenjian <com.me@foxmail.com>
2021-10-30 17:24:50 +0800

    Update betterdummy from 1.0.5 to 1.0.7

--- Casks/betterdummy.rb
+++ Casks/betterdummy.rb
@@ -1,11 +1,16 @@
 cask "betterdummy" do
-  version "1.0.5"
-  sha256 "cbadc6dd7f9630112b39b54b239346db7abbdbc6188859b81ef7a09939feee8e"
+  version "1.0.7"
+  sha256 "0c0fd120da2d53c2f9453a40768f796d1052ba848c73ea9f4c2ecff7874431c1"
 
   url "https://github.com/waydabber/BetterDummy/releases/download/v#{version}/BetterDummy-v#{version}.zip"
   name "BetterDummy"
-  desc "hidpi"
+  desc "Dummy Display for Apple Silicon Macs to achieve custom resolutions"
   homepage "https://github.com/waydabber/BetterDummy"
 
   app "BetterDummy.app"
+
+  zap trash: [
+    "~/Library/Containers/me.waydabber.BetterDummy",
+    "~/Library/Application Scripts/me.waydabber.BetterDummy",
+  ]
 end
