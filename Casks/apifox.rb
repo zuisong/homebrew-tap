39cc431acbbcec30c5d13abfcadff240af2ae950
zuisong <com.me@foxmail.com>
2021-12-27 11:06:33 +0800

    Update apifox(add zap)

--- Casks/apifox.rb
+++ Casks/apifox.rb
@@ -1,11 +1,17 @@
 cask "apifox" do
   version "1.4"
   sha256 :no_check
-  desc "API 文档、API 调试、API Mock、API 自动化测试"
 
   url "https://cdn.apifox.cn/download/Apifox-macOS-arm64-latest.zip"
   name "apifox"
+  desc "API 文档、API 调试、API Mock、API 自动化测试"
   homepage "https://www.apifox.cn/"
 
   app "Apifox.app"
+
+  zap trash: [
+    "Library/Preferences/cn.apifox.app.plist",
+    "Library/Saved Application State/cn.apifox.app.savedState",
+    "Library/Application Support/apifox",
+  ]
 end
