faf97041b98cf465c40a182e0e901359a4458912
zuisong <com.me@foxmail.com>
2022-04-24 22:56:12 +0800

    Update tomatobar from 3.2.0 to 3.3.0

--- Casks/tomatobar.rb
+++ Casks/tomatobar.rb
@@ -1,6 +1,6 @@
 cask "tomatobar" do
-  version "3.2.0"
-  sha256 "130a5680593f0480ebbf539c979f27f3fbaae45de51879ac14f59bec72fcfa25"
+  version "3.3.0"
+  sha256 "ff4d465f0d70cb2e234918a650d3844352d0179e9f3742666ae98d909988dc23"
 
   url "https://github.com/ivoronin/TomatoBar/releases/download/v#{version}/TomatoBar-v#{version}.zip"
   name "TomatoBar"
