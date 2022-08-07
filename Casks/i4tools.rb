4670aaef331e6bd80cc0b3f1f22d1c43df7a378b
zuisong <com.me@foxmail.com>
2022-08-07 11:38:09 +0800

    update i4tools

--- Casks/i4tools.rb
+++ Casks/i4tools.rb
@@ -1,8 +1,8 @@
 cask "i4tools" do
-  version "1.13.010"
-  sha256 "d7e3116e478258dd946788f7a3774f8b71303383d612b2d105bf92f72cab146e"
+  version "1.15.009,20220714"
+  sha256 "3af74916d89ef5d4871e8b0991d2f8cde421e9a75484c799894da8fb41d9005c"
 
-  url "https://d-updater.i4.cn/i4mac/20220418/0/i4Tools_v#{version}.dmg"
+  url "https://d-updater.i4.cn/i4mac/#{version.csv.second}/0/i4Tools_v#{version.csv.first}.dmg"
   name "i4Tools"
   name "爱思助手"
   desc "Mobile device management software"
@@ -10,8 +10,12 @@ cask "i4tools" do
 
   livecheck do
     url "https://url.i4.cn/fqIBBbaa"
-    strategy :header_match
-    regex(/i4Tools[._-]v?(\d+(?:\.\d+)+)\.dmg/i)
+    strategy :header_match do |headers|
+      match = headers["location"].match(%r{/i4mac/([\d.]+)/.+/i4Tools_v([.\d]+)\.dmg}i)
+      next if match.blank?
+
+      "#{match[2]},#{match[1]}"
+    end
   end
 
   auto_updates true
