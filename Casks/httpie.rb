364078cf7ee454ff21324b616de7b929bb228cbe
zuisong <com.me@foxmail.com>
2022-08-15 12:09:30 +0800

    Update httpie from 2022.11.1 to 2022.12.0

--- Casks/httpie.rb
+++ Casks/httpie.rb
@@ -1,10 +1,10 @@
 cask "httpie" do
-  version "2022.11.1"
-  sha256 "e7ca23026b6336248d236ebd13c46fec0aacda95ca06f0a1493764c8ddacd2f3"
+  version "2022.12.0"
+  sha256 "934f08b885c7fa483dcf3ee83a569224d9a199a1c86917544024daaad16890cc"
 
   url "https://github.com/httpie/desktop/releases/download/v#{version}/HTTPie-#{version}-arm64.dmg"
   name "httpie"
-  desc ""
+  desc "Http client"
   homepage "https://github.com/httpie/desktop"
 
   app "HTTPie.app"
