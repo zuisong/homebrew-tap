9f5cac5de9cb0a78cceae716435cc803c28d9721
zuisong <com.me@foxmail.com>
2023-01-16 22:47:39 +0800

    Update clash-verge from 1.2.1 to 1.2.2

--- Casks/clash-verge.rb
+++ Casks/clash-verge.rb
@@ -1,9 +1,9 @@
 cask "clash-verge" do
   arch arm: "aarch64", intel: "x64"
 
-  version "1.2.1"
-  sha256 arm:   "e86bd762f14818a68958814b1599adde3e12111f434e9b750dd80a4ae040f3ae",
-         intel: "1289c836a4258b038e7e5c092fed366a890dabda19fb6645fde621cc71c8a603"
+  version "1.2.2"
+  sha256 arm:   "e46790e59ba416d2513e24ce5d944d49287f4ff8fa82f3edb72b37e91e3fe24d",
+         intel: "7ca2940d0af710de1cbfc85279dc9c2a13f9ee479774a35df689d222292abfa4"
 
   url "https://github.com/zzzgydi/clash-verge/releases/download/v#{version}/Clash.Verge_#{version}_#{arch}.dmg"
   name "Clash for Windows"
