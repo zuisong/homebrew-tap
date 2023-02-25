806674283fed736160afe759df5efdf81bad864d
zuisong <com.me@foxmail.com>
2023-02-25 22:08:06 +0800

    Update ffmpeg-static from 5 to 6

--- Casks/ffmpeg-static.rb
+++ Casks/ffmpeg-static.rb
@@ -1,11 +1,19 @@
 cask "ffmpeg-static" do
-  version "5.0.1"
-  sha256 "6ff886d73aace493ea0b37625d9503ef7493a4a3d0331305ee89c91528b4e3bc"
+  arch arm: "arm", intel: "intel"
 
-  url "https://github.com/eugeneware/ffmpeg-static/releases/download/b#{version}/darwin-arm64.gz"
+  version "6"
+  sha256 arm:   "15e67ff413d3d2436ddb3efd282344e50b8f1c6f834979b984371b90ebaf0449",
+         intel: "4c3623c2356459d17375410d8b3b598b972e20c27131389d123e3f9470793dee"
+
+  url "https://www.osxexperts.net/ffmpeg#{version}#{arch}.zip"
   name "ffmpeg-static"
   desc "Static build of ffmpeg"
-  homepage "https://github.com/eugeneware/ffmpeg-static"
+  homepage "https://www.osxexperts.net/"
 
-  binary "darwin-arm64", target: "ffmpeg"
+  livecheck do
+    url :homepage
+    regex(%r{/ffmpeg([\d_.]+)#{arch}.zip})
+  end
+
+  binary "ffmpeg"
 end
