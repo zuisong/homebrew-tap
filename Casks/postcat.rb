fc85f65e0652a1e1ac0b82392074fd6e7063a434
zuisong <com.me@foxmail.com>
2023-03-23 13:34:17 +0800

    Update postcat from 0.4.0 to 0.4.1

--- Casks/postcat.rb
+++ Casks/postcat.rb
@@ -1,9 +1,9 @@
 cask "postcat" do
   arch arm: "-arm64"
 
-  version "0.4.0"
-  sha256 arm:   "759ec5a234459cd1c4c5e63ccda94374dab29014b7e57fcd85a5d6b2f24470c2",
-         intel: "1ff8431ba516302de22bd6764c0d0c900a7eccb42883419e7e0273368f4c1ba5"
+  version "0.4.1"
+  sha256 arm:   "6cdb3e98d3495bce7e51b4e2b7de806cf78b4acbf940cf74e801a74a693a6501",
+         intel: "5f3e3c8c4217856f90a203fee7e145908097934dd9fef1fd09d836f62324591f"
 
   url "https://github.com/Postcatlab/postcat/releases/download/v#{version}/Postcat-#{version}#{arch}-mac.zip"
   name "Postcat"
