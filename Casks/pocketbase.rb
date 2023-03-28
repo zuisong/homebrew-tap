4d553531723eb939790e626d3fca0b54d34dc067
zuisong <com.me@foxmail.com>
2023-03-28 09:42:50 +0800

    Update pocketbase from 0.13.3 to 0.13.4

--- Casks/pocketbase.rb
+++ Casks/pocketbase.rb
@@ -3,7 +3,7 @@ cask "pocketbase" do
 
   version "0.13.4"
   sha256 arm:   "56b7a35d0ab8aafb678dbb545bd2f85f0240714bb19e725c603b727cf3813632",
-         intel: "1c1c2ec6cb1b3fa8aa2bd5ddfbafefed6eb3db0dcbb1fda2bcdf9ceacab7bf41"
+         intel: "b0cbce4850859e75e8c64a46672c8cf71d8c01ceb024faecb105108d507e7388"
 
   url "https://github.com/pocketbase/pocketbase/releases/download/v#{version}/pocketbase_#{version}_darwin_#{arch}.zip"
   name "PocketBase"
