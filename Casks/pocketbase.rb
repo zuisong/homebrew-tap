2d54cbb0a28ec8ddd97ee177eda44348583abbfd
zuisong <com.me@foxmail.com>
2023-04-18 14:24:17 +0800

    pocketbase 0.15.0

--- Casks/pocketbase.rb
+++ Casks/pocketbase.rb
@@ -1,9 +1,9 @@
 cask "pocketbase" do
   arch arm: "arm64", intel: "amd64"
 
-  version "0.14.5"
-  sha256 arm:   "959030f2298df85643406e018f757afc1b43d9dfe5f04ad3c2a34231a6a8d271",
-         intel: "0c5cf2df43b65953c270e7479074a3f61cb6c20fc331cd47eb6fc03a5394c108"
+  version "0.15.0"
+  sha256 arm:   "2d26ae662836ed64550233bf07c62d436ad87e875033971e281038fddfe35230",
+         intel: "b4f7f0715d90f2f44d2587f1dda3197ff423924179f82289af58bff4455a4d6c"
 
   url "https://github.com/pocketbase/pocketbase/releases/download/v#{version}/pocketbase_#{version}_darwin_#{arch}.zip"
   name "PocketBase"
