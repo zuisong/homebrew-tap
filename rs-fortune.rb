9ab2c14510e811724a2a8281fd013c991891556a
zuisong <com.me@foxmail.com>
2023-05-28 21:44:36 +0800

    rs-fortune 0.4.0

--- rs-fortune.rb
+++ rs-fortune.rb
@@ -1,8 +1,8 @@
 class RsFortune < Formula
   desc "Rust version of fortune"
   homepage "https://github.com/zuisong/rs-fortune"
-  url "https://github.com/zuisong/rs-fortune/archive/refs/tags/v0.3.0.tar.gz"
-  sha256 "fea3cf2180a94ab9d6aef7a08ade53aa510ad03dfa74d732f7bafacd27daf9da"
+  url "https://github.com/zuisong/rs-fortune/archive/refs/tags/v0.4.0.tar.gz"
+  sha256 "d500a8d9f061cf9c4afec179d11f73eec2092609fa13b78a9420c9773c2919e1"
   license "MIT"
 
   depends_on "rust" => :build
