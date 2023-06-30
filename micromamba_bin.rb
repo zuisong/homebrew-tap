aae03493d9943f95fc86625cc750ea510a65dd97
zuisong <com.me@foxmail.com>
2023-06-30 16:46:14 +0800

    micromamba_bin 1.4.5

--- micromamba_bin.rb
+++ micromamba_bin.rb
@@ -1,9 +1,9 @@
 class MicromambaBin < Formula
   desc "Fast Cross-Platform Package Manager"
   homepage "https://github.com/mamba-org/mamba"
-  url "https://api.anaconda.org/download/conda-forge/micromamba/1.4.4/osx-arm64/micromamba-1.4.4-0.tar.bz2"
-  version "1.4.4"
-  sha256 "642e770787573d33568b2befdc8c6418d75fc402df95b720232640b27d31fa2d"
+  url "https://api.anaconda.org/download/conda-forge/micromamba/1.4.5/osx-arm64/micromamba-1.4.5-0.tar.bz2"
+  version "1.4.5"
+  sha256 "250d00499ac00207da4cb7a9969e6ceac0ddd3debffb9a3f0fa3dedf560d8e76"
   license "BSD-3-Clause"
 
   livecheck do
