bda72971eca15e34785f0895c6f29cb60528707b
zuisong <com.me@foxmail.com>
2023-07-07 14:28:01 +0800

    clash-verge 1.3.3

--- Casks/clash-verge.rb
+++ Casks/clash-verge.rb
@@ -1,9 +1,9 @@
 cask "clash-verge" do
   arch arm: "aarch64", intel: "x64"
 
-  version "1.3.0"
-  sha256 arm:   "e277e4b93f7ac10d6e92afcc34e37fb6d235e5d4ce372988c804b90b0c6b0668",
-         intel: "6ec2e7432269a41f901a8f1a6eb15bc30e3cfb039f57381e3e55832c4e2e500f"
+  version "1.3.3"
+  sha256 arm:   "188b5668052ce170f297e9fc83240f78ee4f745d8cdbccc767f01844c72a5509",
+         intel: "5930a7aa4ac2bf3341f494c81d703b242d38d6b2802177b74f2ecfb585640c87"
 
   url "https://github.com/zzzgydi/clash-verge/releases/download/v#{version}/Clash.Verge_#{version}_#{arch}.dmg"
   name "Clash for Windows"
