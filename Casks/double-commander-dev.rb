10cd97db14cb5d12c7a27b064a795ba7060794ab
zuisong <com.me@foxmail.com>
2023-07-13 00:32:07 +0800

    double-commander-dev 1.1.0-10804

--- Casks/double-commander-dev.rb
+++ Casks/double-commander-dev.rb
@@ -1,9 +1,9 @@
 cask "double-commander-dev" do
   arch arm: "aarch64", intel: "x86_64"
 
-  version "1.1.0-10783"
-  sha256 arm:   "b3455ba70f008b4e402c1372be27f9c3c31a7c07a3da5aa5cd309eedc472f4ae",
-         intel: "d924eadde210fbdd06d44fbe8e3e9613785a5b7351ee1f1f74e2666e15d71066"
+  version "1.1.0-10804"
+  sha256 arm:   "3bfc1b4b19722c86a546e9e30500f1eb4295b88dcdac61a2ed51f9f418714902",
+         intel: "e16544a9681ba9ec108801b5040e168456514698f5c66c43f2b148ed60a164cd"
 
   # audit ignore "SourceForge URL format incorrect".
   url "https://doublecmd.sourceforge.io/snapshots/doublecmd-#{version}.cocoa.#{arch}.dmg"
