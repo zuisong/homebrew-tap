68bd91cfd9f19818f0ad1d9de52aa69dc9dc43a1
zuisong <com.me@foxmail.com>
2023-08-28 17:00:41 +0800

    rs-fortune 0.7.0

--- rs-fortune.rb
+++ rs-fortune.rb
@@ -1,13 +1,13 @@
 class RsFortune < Formula
   desc "Rust version of fortune"
   homepage "https://github.com/zuisong/rs-fortune"
-  url "https://github.com/zuisong/rs-fortune/releases/download/v0.6.0/rs-fortune-universal-apple-darwin.tar.gz"
-  sha256 "578fc917af3742864dcd886748f3cdbb1b1bf62566ec13bb49178e923fc854e7"
+  url "https://github.com/zuisong/rs-fortune/releases/download/v0.7.0/rs-fortune-universal-apple-darwin.tar.gz"
+  sha256 "e9cffe647bbbe897c29aeef1412a7bd3eecff2208d7e771191cce4b026fc9b5f"
   license "MIT"
 
   def install
     bin.install "rs-fortune"
-    generate_completions_from_executable(bin/"rs-fortune", "--completion")
+    generate_completions_from_executable(bin/"rs-fortune", "completions", "--shell")
   end
 
   test do
