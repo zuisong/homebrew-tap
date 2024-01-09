fde4e98abd3e2759aa5ac02516accc5db25ddb09
zuisong <com.me@foxmail.com>
2024-01-09 22:53:09 +0800

    ffmpeg-static 6.0

--- Casks/ffmpeg-static.rb
+++ Casks/ffmpeg-static.rb
@@ -1,21 +1,13 @@
 cask "ffmpeg-static" do
-  arch arm: "arm", intel: "intel"
+  arch arm: "arm64", intel: "x64"
 
-  version "611"
-  sha256 arm:   "da124a70feedaf1d1972cc21e1038194974393167472c0efdda4de3f87610d7e",
-         intel: "94dcde746364b516d4c9d79cbae2807e5c0bea6a1a8be45742c2cd6850303426"
+  version "6.0"
+  sha256 "6be74d6f449889c2e87a75873894f8520cad56c08ac76f2a628d85b0519daaca"
 
-  url "https://www.osxexperts.net/ffmpeg#{version}#{arch}.zip"
+  url "https://github.com/eugeneware/ffmpeg-static/releases/download/b#{version}/ffmpeg-darwin-#{arch}.gz"
   name "ffmpeg-static"
   desc "Static build of ffmpeg"
-  homepage "https://www.osxexperts.net/"
+  homepage "https://github.com/eugeneware/ffmpeg-static"
 
-  livecheck do
-    url :homepage
-    regex(%r{/ffmpeg([\d_.]+)#{arch}.zip})
-  end
-
-  conflicts_with formula: "ffmpeg"
-
-  binary "ffmpeg"
+  binary "ffmpeg-darwin-#{arch}", target: "ffmpeg"
 end
