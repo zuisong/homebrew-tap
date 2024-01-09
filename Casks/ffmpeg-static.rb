f8a1f3b6104abdddd134586a1d8ff71224d689c4
zuisong <com.me@foxmail.com>
2024-01-09 22:53:09 +0800

    ffmpeg-static 6.0

--- Casks/ffmpeg-static.rb
+++ Casks/ffmpeg-static.rb
@@ -19,3 +19,17 @@ cask "ffmpeg-static" do
 
   binary "ffmpeg"
 end
+
+cask "ffmpeg-static" do
+  arch arm: "arm64", intel: "x64"
+
+  version "6.0"
+  sha256 "6be74d6f449889c2e87a75873894f8520cad56c08ac76f2a628d85b0519daaca"
+
+  url "https://github.com/eugeneware/ffmpeg-static/releases/download/b#{version}/ffmpeg-darwin-#{arch}.gz"
+  name "ffmpeg-static"
+  desc "Static build of ffmpeg"
+  homepage "https://github.com/eugeneware/ffmpeg-static"
+
+  binary "darwin-arm64", target: "ffmpeg"
+end
