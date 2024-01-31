d89c4c33b588f65b5c4ffb38c65280f4f6e13624
zuisong <com.me@foxmail.com>
2024-01-31 21:03:09 +0800

    rubick 4.2.1

--- Casks/rubick.rb
+++ Casks/rubick.rb
@@ -1,9 +1,9 @@
 cask "rubick" do
   arch arm: "arm64", intel: "x64"
 
-  version "4.1.6"
-  sha256 arm:   "f5c97e2ec26b695719dd31c97855c268904eb088f352266a8ef83f99ef2730ad",
-         intel: "afc935cacad21c29c8a4580521fd669788bbb1151bf3292f71406887f88e0a86"
+  version "4.2.1"
+  sha256 arm:   "011dfe1be132a239f2cb81f6dab4fde144e11a4cb0a424b2c8c08842f56e85db",
+         intel: "0bf8b93868c542a76e21cbf444aaabc3b9421820c69cb78036c8e2296872fb85"
 
   url "https://github.com/rubickCenter/rubick/releases/download/v#{version}/rubick-#{version}-#{arch}.dmg"
   name "rubick"
@@ -18,9 +18,9 @@ cask "rubick" do
   app "rubick.app"
 
   zap trash: [
-    "~/rubick",
     "~/Library/Application Support/rubick/",
     "~/Library/Preferences/com.muwoo.rubick.plist",
     "~/Library/Services/rubick.workflow/",
+    "~/rubick",
   ]
 end
