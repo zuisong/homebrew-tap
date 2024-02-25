a6193bb73360d75bfa687f21b41e0733d0e29540
zuisong <com.me@foxmail.com>
2024-02-25 21:57:59 +0800

    xterminal 1.20.0

--- Casks/xterminal.rb
+++ Casks/xterminal.rb
@@ -1,9 +1,9 @@
 cask "xterminal" do
   arch arm: "arm64", intel: "x64"
 
-  version "1.8.0"
-  sha256 arm:   "d453176066bac518f2bcb907b343e23bfb42e6eb2d17b97e48782438a7a4b97d",
-         intel: "68634341d0770f982eb0076ab865163c28b1f1e0e0746b899e077c93c341eb7b"
+  version "1.20.0"
+  sha256 arm:   "68e069f7ca82e9c8036910f4152a1c9d90d3ddd11b7cbea5be2e7f5fa523267b",
+         intel: "9f83f5b94e4485bd539b755a388745225e27d1ce2c970265af79b1767a1735f2"
 
   url "https://cdn-cn.xterminal.cn/xterminal/XTerminal-#{version}-mac-#{arch}.dmg"
   name "xterminal"
