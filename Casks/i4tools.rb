f96a8883ef7ac098732485ecfd70d9e32330d960
zuisong <com.me@foxmail.com>
2024-03-20 22:51:31 +0800

    i4tools 3.03.007

--- Casks/i4tools.rb
+++ Casks/i4tools.rb
@@ -1,6 +1,6 @@
 cask "i4tools" do
-  version "3.03.005,20240315,0"
-  sha256 "e78006017f027129cb8ee89ee11dcf84aef32fae79f21a731c7be2e6e767d01c"
+  version "3.03.007,20240318,0"
+  sha256 "a846bedfb30ceb660c43c23b03c648204dffd055ffef445c7a098720d11edf4d"
 
   url "https://d-updater.i4.cn/i4mac/#{version.csv.second}/#{version.csv.third}/i4Tools_v#{version.csv.first}.dmg"
   name "i4Tools"
