27574fb950d1dd34d5fa6dc9a6934c801a800870
zuisong <com.me@foxmail.com>
2024-03-26 23:58:33 +0800

    micromamba_bin 1.5.8

--- micromamba_bin.rb
+++ micromamba_bin.rb
@@ -1,9 +1,9 @@
 class MicromambaBin < Formula
   desc "Fast Cross-Platform Package Manager"
   homepage "https://github.com/mamba-org/mamba"
-  url "https://api.anaconda.org/download/conda-forge/micromamba/1.5.7/osx-arm64/micromamba-1.5.7-0.tar.bz2"
-  version "1.5.7"
-  sha256 "3c1b263e29a68b74843be2bb3e2b5b76950d37eaa32a460556ef90bc24375176"
+  url "https://api.anaconda.org/download/conda-forge/micromamba/1.5.8/osx-arm64/micromamba-1.5.8-0.tar.bz2"
+  version "1.5.8"
+  sha256 "d62bdc8179a485b931007d623f299ef307cdfba45438fc1f4a8d055ddc232ee1"
   license "BSD-3-Clause"
 
   livecheck do
