1de199e42798e3eb6b3d0cf8d4ddd8cada4c5b9e
zuisong <com.me@foxmail.com>
2024-04-24 12:37:31 +0800

    dotfiles 0.2.3

--- dotfiles.rb
+++ dotfiles.rb
@@ -1,22 +1,13 @@
 class Dotfiles < Formula
   desc "Symbolic links management CLI"
   homepage "https://github.com/rhysd/dotfiles"
-  url "https://github.com/rhysd/dotfiles/archive/v0.2.2.tar.gz"
-  sha256 "090b918636b405be8dab6d19e541afda2bf0e252409bff284679b297d3e563e4"
+  url "https://github.com/rhysd/dotfiles/archive/refs/tags/v0.2.3.tar.gz"
+  sha256 "fd98beaf3a8372272f7218bfd0bea578e9bfea6ae01e3a8b260e92fc35a2434b"
   license "MIT"
-
-  livecheck do
-    url :url
-    strategy :github_latest
-  end
 
   depends_on "go" => :build
 
   def install
     system "go", "build", *std_go_args, "."
-  end
-
-  test do
-    system "dotfiles", "version"
   end
 end
