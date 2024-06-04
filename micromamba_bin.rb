95eb0dd57d4254f43abbe6ff1f5180771178c958
zuisong <com.me@foxmail.com>
2024-06-04 13:16:28 +0800

    micromamba_bin 1.5.8-0

--- micromamba_bin.rb
+++ micromamba_bin.rb
@@ -1,17 +1,10 @@
 class MicromambaBin < Formula
   desc "Fast Cross-Platform Package Manager"
   homepage "https://github.com/mamba-org/mamba"
-  url "https://api.anaconda.org/download/conda-forge/micromamba/1.5.8/osx-arm64/micromamba-1.5.8-0.tar.bz2"
-  version "1.5.8"
+  url "https://github.com/mamba-org/micromamba-releases/releases/download/1.5.8-0/micromamba-osx-arm64.tar.bz2"
+  version "1.5.8-0"
   sha256 "d62bdc8179a485b931007d623f299ef307cdfba45438fc1f4a8d055ddc232ee1"
   license "BSD-3-Clause"
-
-  livecheck do
-    url "https://micro.mamba.pm/api/micromamba/osx-arm64/latest"
-    strategy :header_match do |headers|
-      headers["location"][/micromamba-([\d.]+)-\d+\.tar\..+/i, 1]
-    end
-  end
 
   def install
     bin.install "bin/micromamba"
