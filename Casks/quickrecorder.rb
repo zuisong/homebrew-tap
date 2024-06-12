54ce368fe8d1686fe306527dac1712347203f344
zuisong <com.me@foxmail.com>
2024-06-12 14:01:43 +0800

    quickrecorder 1.3.1

---
+++ Casks/quickrecorder.rb
@@ -0,0 +1,18 @@
+cask "quickrecorder" do
+  version "1.3.1"
+  sha256 "73f18cc37325e2ef9b2f6912b1a69d176d86cb1d331cc1ab0abd728b4404d7ef"
+
+  url "https://github.com/lihaoyun6/QuickRecorder/releases/download/#{version}/QuickRecorder_v#{version}.dmg"
+  name "QuickRecorder"
+  desc "Lightweight and high-performance screen recorder"
+  homepage "https://github.com/lihaoyun6/QuickRecorder"
+
+  depends_on macos: ">= :monterey"
+
+  app "QuickRecorder.app"
+
+  zap trash: [
+    "~/Library/HTTPStorages/com.lihaoyun6.QuickRecorder",
+    "~/Library/Preferences/com.lihaoyun6.QuickRecorder.plist",
+  ]
+end
