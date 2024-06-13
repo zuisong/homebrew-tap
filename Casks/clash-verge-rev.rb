4eeeb19adc8505de511c8a000e9f0b9ecacf822a
zuisong <com.me@foxmail.com>
2024-06-13 16:54:08 +0800

    clash-verge-rev 1.6.6

--- Casks/clash-verge-rev.rb
+++ Casks/clash-verge-rev.rb
@@ -1,9 +1,9 @@
 cask "clash-verge-rev" do
   arch arm: "aarch64", intel: "x64"
 
-  version "1.6.5"
-  sha256 arm:   "911a1797d2762fb59e75d35a9798cd69ad1749d6200898d55adbc5deef1d271e",
-         intel: "98642ee60bb6b9e2944432d333a22598f2cff8ed1cf67d1cb5d87c94b94cd299"
+  version "1.6.6"
+  sha256 arm:   "b2e9759533fc253617a54d75a185436146cf9da6c9d101997f0e1b962f7ae573",
+         intel: "90eb3a5f5a8175d73e63616e4e9afff7db4b438924b9854963bf8f06230c9cc8"
 
   url "https://github.com/clash-verge-rev/clash-verge-rev/releases/download/v#{version}/Clash.Verge_#{version}_#{arch}.dmg",
       verified: "github.com/clash-verge-rev/clash-verge-rev/"
