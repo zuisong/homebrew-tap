26bb58d8a0d7478d0872c909b40233e7c6ef4942
zuisong <com.me@foxmail.com>
2024-06-18 12:49:51 +0800

    quickrecorder 1.3.7

--- Casks/quickrecorder.rb
+++ Casks/quickrecorder.rb
@@ -1,6 +1,6 @@
 cask "quickrecorder" do
-  version "1.3.5"
-  sha256 "bc4288018fa14935d309d8654dc3692b42b71e31338af676fd2fa925cfcd4528"
+  version "1.3.7"
+  sha256 "40aae1d6401e5bb4dab3b0e7f6f4a769648bd248ee0bec4698aeba54af1970a9"
 
   url "https://github.com/lihaoyun6/QuickRecorder/releases/download/#{version}/QuickRecorder_v#{version}.dmg"
   name "QuickRecorder"
