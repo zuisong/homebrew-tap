70fb3da86d32172374bab1d63b2050f2ab3570f4
zuisong <com.me@foxmail.com>
2024-09-21 16:52:11 +0800

    go2tv 1.17.0

--- go2tv.rb
+++ go2tv.rb
@@ -1,8 +1,8 @@
 class Go2tv < Formula
   desc "Cast your media files to UPnP/DLNA Media Renderers and Smart TVs"
   homepage "https://github.com/alexballas/go2tv"
-  url "https://github.com/alexballas/go2tv/archive/refs/tags/v1.16.1.tar.gz"
-  sha256 "1c8001ff6fa8f35c046a29130c22ccfef49ecf5557c50c206fab4ee5d66b6f94"
+  url "https://github.com/alexballas/go2tv/archive/refs/tags/v1.17.0.tar.gz"
+  sha256 "2bdac54b3c98e39cb3ddc28db7f2c841be4cc3d7e6ec710263881db6920a5d32"
   license "MIT"
 
   livecheck do
