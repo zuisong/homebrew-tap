6f247652e92438a0c3916437b3f6b7a875a5bafe
zuisong <com.me@foxmail.com>
2024-10-01 23:01:16 +0800

    micromamba-bin 2.0.1-0

--- Casks/micromamba-bin.rb
+++ Casks/micromamba-bin.rb
@@ -1,9 +1,9 @@
 cask "micromamba-bin" do
   arch arm: "arm64", intel: "64"
 
-  version "1.5.10-0"
-  sha256 arm:   "e34c48f3c7c3c0855ea9c5f82ad6ffb316daecbddad3d9032c2a2d782501d7b0",
-         intel: "3b5dc32ed12d964392a4cf869a3d5dd0aaa7f5fb917a7c0831c35ddd323438ea"
+  version "2.0.1-0"
+  sha256 arm:   "05a3036b0761baada4f0e26f2a6a189edc301845960f50eb3eb844a543c0cf18",
+         intel: "fd7809fff6c1525f0cc4b9f811050a767940dbb7d92330b1094b536032f87113"
 
   url "https://github.com/mamba-org/micromamba-releases/releases/download/#{version}/micromamba-osx-#{arch}.tar.bz2"
   name "micromamba"
