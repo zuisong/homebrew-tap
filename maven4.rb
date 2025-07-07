b24ab8b6469c6262a0c9b4527b793ce8fc00e388
zuisong <com.me@foxmail.com>
2025-07-07 13:54:35 +0800

    maven4 4.0.0-rc-4

--- maven4.rb
+++ maven4.rb
@@ -1,9 +1,9 @@
 class Maven4 < Formula
   desc "Java-based project management"
   homepage "https://maven.apache.org/"
-  url "https://www.apache.org/dyn/closer.lua?path=maven/maven-4/4.0.0-rc-3/binaries/apache-maven-4.0.0-rc-3-bin.tar.gz"
-  version "4.0.0-rc-3"
-  sha256 "ef86d972e52a04866f5f78b457d21d7ecd96efa99c696998f2bd4b86ee020bcd"
+  url "https://www.apache.org/dyn/closer.lua?path=maven/maven-4/4.0.0-rc-4/binaries/apache-maven-4.0.0-rc-4-bin.tar.gz"
+  version "4.0.0-rc-4"
+  sha256 "726b1f7fcaa6d574d647c1f5c4d24829942bd0cd88b5924c9aac0794d8bed65b"
   license "Apache-2.0"
 
   livecheck do
