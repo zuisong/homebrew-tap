9fc4ea67b85e01f9218c6ff5cb1667065442adf6
zuisong <19145952+zuisong@users.noreply.github.com>
2025-08-01 18:02:25 +0800

    tinyrdm 1.2.4 (#278)

--- Casks/tinyrdm.rb
+++ Casks/tinyrdm.rb
@@ -1,9 +1,9 @@
 cask "tinyrdm" do
   arch arm: "arm64", intel: "intel"
 
-  version "1.2.3"
-  sha256 arm:   "ba7bdb29f2194533366199231c3f3971a3c128704070c53d6ee4ad78241e4140",
-         intel: "adf1bda0b91bf8826c68b8ce9c5ae19de3b15af5ccfbdfe4009f678f3ed54962"
+  version "1.2.4"
+  sha256 arm:   "e3a7e639c667159d49b2937074bd40988725d7a7629f89103d777b937b304cfa",
+         intel: "fb995f97fec8352aa1c91e508c35c9c0da6335ff0c84b87fedb7ee70fa00ec20"
 
   url "https://github.com/tiny-craft/tiny-rdm/releases/download/v#{version}/TinyRDM_#{version}_mac_#{arch}.dmg"
   name "tinyrdm"
