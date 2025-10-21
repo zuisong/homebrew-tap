e66c2dc51052e6879fb8665449e79ea9e537af1d
zuisong <19145952+zuisong@users.noreply.github.com>
2025-10-21 17:29:30 +0800

    i4tools 3.22.006 (#287)

--- Casks/i4tools.rb
+++ Casks/i4tools.rb
@@ -1,6 +1,6 @@
 cask "i4tools" do
-  version "3.21.002,20250722,0"
-  sha256 "da787396fa948c71858be9ac580e343bc26908226e2079e51a5e40acde62137b"
+  version "3.22.006,20250922,0"
+  sha256 "9aad483d066b2457194c10c41e64e486e675152f66d569a30e8829f7902dbf63"
 
   url "https://d-app6.i4.cn/i4tools/mac/#{version.csv.second}/#{version.csv.third}/i4Tools_v#{version.csv.first}.dmg"
   name "i4Tools"
@@ -11,7 +11,7 @@ cask "i4tools" do
   livecheck do
     url "https://url.i4.cn/fqIBBbaa"
     strategy :header_match do |headers|
-      match = headers["location"].match(%r{/([\d.]+)/(\d)/i4Tools[\D]+([.\d]+)\.dmg}i)
+      match = headers["location"].match(%r{/([\d.]+)/(\d)/i4Tools\D+([.\d]+)\.dmg}i)
       next if match.blank?
 
       "#{match[3]},#{match[1]},#{match[2]}"
