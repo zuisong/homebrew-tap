3b6b24ddf4c6880aa1194c68a41421797a45feaa
zuisong <com.me@foxmail.com>
2025-11-10 20:10:07 +0800

    gpgosx

--- Casks/gnupg.rb
+++ Casks/gnupg.rb
@@ -7,8 +7,6 @@ cask "gnupg" do
   desc "Is a complete and free implementation of the OpenPGP standard"
   homepage "https://sourceforge.net/projects/gpgosx/"
 
-  depends_on macos: ">= :sierra"
-
   pkg "Install.pkg"
 
   uninstall pkgutil: "net.sourceforge.gpgosx",
