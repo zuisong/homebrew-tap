c9eec23bd2161b1a6b2a4c7ffb089c199a385a96
zuisong <com.me@foxmail.com>
2026-03-23 15:12:10 +0800

    tinyrdm 1.2.6

--- Casks/tinyrdm.rb
+++ Casks/tinyrdm.rb
@@ -1,9 +1,9 @@
 cask "tinyrdm" do
   arch arm: "arm64", intel: "intel"
 
-  version "1.2.5"
-  sha256 arm:   "0918611c8a96dbbda5a8e59c4c810659bca5033d82f780df865d4d1dfed25b8a",
-         intel: "4415ff1bb28603c2bd18b57700ef6f515098bfd0d6b9b61f5aaccadc378a3c33"
+  version "1.2.6"
+  sha256 arm:   "6a9de72ebb6be5b4fce1ebec3186edf8bd1ed5e3221bede082479c6ffe03a61b",
+         intel: "ac69281ec7d943d9e315a87c152a02ccca3da4cbf030c55db53f2504aa3f7898"
 
   url "https://github.com/tiny-craft/tiny-rdm/releases/download/v#{version}/TinyRDM_#{version}_mac_#{arch}.dmg"
   name "tinyrdm"
@@ -14,6 +14,8 @@ cask "tinyrdm" do
     url :url
     strategy :github_latest
   end
+
+  depends_on macos: ">= :big_sur"
 
   app "Tiny RDM.app"
 
