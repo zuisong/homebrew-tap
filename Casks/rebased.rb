9808b4511d50b14c4f75e29ba68f37478b145065
zuisong <com.me@foxmail.com>
2026-05-09 11:32:10 +0800

    rebased 1.0.12

--- Casks/rebased.rb
+++ Casks/rebased.rb
@@ -1,24 +1,17 @@
 cask "rebased" do
-  version "1.0.8,261.22158.SNAPSHOT"
-  sha256 "19f4ddc9807737b56510177a8e1e20a98bba796e67caf086fa3c90c29b3129b8"
+  version "1.0.12"
+  sha256 "3ef9aace106128e78e94777c7fe64228cfa1df816e7cc15b8b1bc054b7df9e9c"
 
-  url "https://github.com/DetachHead/rebased/releases/download/#{version.csv.first}/ideaIC-#{version.csv.second}-aarch64.dmg"
+  url "https://github.com/DetachHead/rebased/releases/download/#{version}/rebased-aarch64.dmg"
   name "Rebased"
   desc "Open-source remake of the JetBrains Git client"
   homepage "https://github.com/DetachHead/rebased"
 
   livecheck do
-    url :url
-    strategy :github_latest do |json, regex|
-      json["assets"]&.filter_map do |asset|
-        match = asset["browser_download_url"]&.match(regex)
-        next if match.blank?
-
-        "#{match[1]},#{match[2]}"
-      end
-    end
-    regex(%r{/download/([^/]+)/ideaIC-([0-9]+(?:\.[0-9]+)+(?:\.SNAPSHOT)?)-aarch64\.dmg$}i)
+    strategy :github_latest
   end
+
+  depends_on arch: :arm64
 
   app "Rebased.app"
   shimscript = "#{staged_path}/rebased.wrapper.sh"
