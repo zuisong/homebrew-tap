7828c518d5568e211d7239ec1ec417164fe7ef10
zuisong <com.me@foxmail.com>
2026-05-18 09:30:51 +0800

    herdr 0.5.10

--- herdr.rb
+++ herdr.rb
@@ -1,9 +1,9 @@
 class Herdr < Formula
   desc "Agent multiplexer that lives in your terminal"
   homepage "https://herdr.dev"
-  url "https://github.com/ogulcancelik/herdr/releases/download/v0.5.9/herdr-macos-aarch64"
-  version "0.5.9"
-  sha256 "8b796f3717862aae81daae5b9d6c1f1929d758aacea0b4433bbf5300ed2ca6ac"
+  url "https://github.com/ogulcancelik/herdr/releases/download/v0.5.10/herdr-macos-aarch64"
+  version "0.5.10"
+  sha256 "84aa7f38c835b4eb5aa38c29660813023cf23178e46541c4b9c260712fe52d51"
   license "AGPL-3.0-or-later"
 
   on_macos do
