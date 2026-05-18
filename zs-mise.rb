076927396cbc678a1e62f72d9874e038e1f65e82
zuisong <com.me@foxmail.com>
2026-05-18 17:03:33 +0800

    zs-mise 2026.5.11

--- zs-mise.rb
+++ zs-mise.rb
@@ -1,9 +1,9 @@
 class ZsMise < Formula
   desc "Dev tools, env vars, task runner"
   homepage "https://mise.en.dev"
-  url "https://github.com/jdx/mise/releases/download/v2026.5.7/mise-v2026.5.7-macos-arm64.tar.xz"
-  version "2026.5.7"
-  sha256 "e3835446d5e120c7fef89ff5a98638b81ac3051037749df6e06574fe040aa3e9"
+  url "https://github.com/jdx/mise/releases/download/v2026.5.11/mise-v2026.5.11-macos-arm64.tar.xz"
+  version "2026.5.11"
+  sha256 "78bc28455c49dd6354c1bb1812735ee7fe7108282984d85546c953a1cd2059c9"
   license "MIT"
 
   depends_on "usage"
