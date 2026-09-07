6bf64a337f61887507d04ed6b26dcbcf5e785ac0
zuisong <com.me@foxmail.com>
2026-09-07 17:34:34 +0800

    codedb

--- codedb.rb
+++ codedb.rb
@@ -1,10 +1,15 @@
 class Codedb < Formula
   desc "Code intelligence server and MCP server"
   homepage "https://github.com/justrach/codedb"
-  url "https://github.com/justrach/codedb/releases/download/v0.2.58181/codedb-darwin-arm64"
-  version "0.2.58181"
-  sha256 "c16fa2a2a6be4eaea0b5a0b98f07113ac243b85a393baabac348266d41ef7096"
+  url "https://github.com/justrach/codedb/releases/download/v0.2.5854/codedb-darwin-arm64"
+  sha256 "92d71bb064d81ff1844103fe4b8748af6ff0d8394a6d43915127df534556a7d2"
   license "BSD-3-Clause"
+  version_scheme 1
+
+  livecheck do
+    url :stable
+    strategy :github_latest
+  end
 
   on_macos do
     depends_on arch: :arm
